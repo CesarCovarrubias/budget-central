837cde6391c1c8028cbaff922292456b35738cbc
cushonz <77802000+cushonz@users.noreply.github.com>
2021-01-27 01:55:12 -0800

    added monthlyIncome method

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,20 +4,38 @@ import { StyleSheet, TextInput, Button } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import Categories from '../components/Categories';
 import { Text, View } from '../components/Themed';
+import Finance from '../components/Categories';
+import { constructor } from 'react';
+
 
 const Income = (props) => {
+  const [enteredAmount] = React.useState(0);
   return (
     <TextInput
       keyboardType = {'number-pad'}
       editable
       placeholder = {'0.00'}
       maxLength={15}
+     // onSubmitEditing = {} Im thinking about using this as the trigger for saving the value to a variable
+
     />
   )
 }
 
-//date time testing
-let date : Date = new Date()
+
+function monthlyIncome(){
+  //early implementation of adding the income value to the current budget based on Date
+  //assuming a biweekly pay period
+  //not perfect as this isnt how pay periods always work but its a start
+  let date : Date = new Date()
+  let day = date.getDate()
+  if (day % 9 == 0 && day > 7)
+  {
+    console.log('Its pay day')
+    //this will eventually add to/recalculate the budget every 7 days
+  }
+}
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
@@ -26,7 +44,7 @@ export default function TabOneScreen() {
       <Income style={styles.container}/>
         <Button 
           title={'Do stuff'}
-          onPress={() => console.log('yay you can do stuff')} //change-income() call here
+          onPress={() => monthlyIncome()} //change-income() call here
         />
 
       <Categories title={'Budgeting Categories'}/>
