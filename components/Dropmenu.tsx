77d874efec503ec57cd1f733c12870b8bfd0e244
Kercheval-S <kerchevals@cwu.edu>
2021-02-26 18:24:11 -0800

    Changed DropDown
    Installed and using React bootstrap for drop down.

--- components/Dropmenu.tsx
+++ components/Dropmenu.tsx
@@ -1,167 +1,22 @@
-//I installed "npm install react-native-dropdown-picker --save" & "npm install --save react-native-vector-icons" & "npm i --save-dev @types/react-native-vector-icons"
-//Git page: https://github.com/hossein-zare/react-native-dropdown-picker#readme
+//npm install react-bootstrap bootstrap
 
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-import DropDownPicker from 'react-native-dropdown-picker';
-import { constructor, useState } from 'react';
-import { Text, View } from 'react-native';
-import * as React from 'react';
-import { renderByOrder } from '../recharts/src/util/ReactUtils';
-import Icon from 'react-native-vector-icons/Feather';
+export default function Dropmenu(){
 
-
-interface DropProps {
-  value: boolean
-}
-
-const [value, setValue] = useState(null);
-const [items, setItems] = useState(null);
-let controller;
-
-export class Dropmenu extends React.Component{
-  controller: any;
-
-  constructor(props: DropProps) {
-    super(props);
-    this.state = {
-      value: null,
-      items: []
-    };
-
-    this.controller;
-  }
-
-  render() {
-
-    return (
-      <View>
-      <DropDownPicker
-        items={[
-          {label: 'Savings', value: 'savings'},
-          {label: 'Recreation', value: 'recreation'},
-          {label: 'Personal', value: 'personal'},
-          {label: 'Housing', value: 'housing'},
-          {label: 'Food', value: 'food'},
-          {label: 'Insurance', value: 'insurance'},
-          {label: 'Medical', value: 'medical'},
-          {label: 'Transport', value: 'transport'}
-        ]}
-        controller={instance => controller = instance}
-        onChangeList={(items, callback) => {
-            new Promise((resolve, reject) => resolve(setItems(items)))
-                .then(() => callback())
-                .catch(() => {});
-        }}
-
-        defaultValue={value}
-        onChangeItem={item => setValue(item.value)}
-      />
-    </View>
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import React, { useState } from "react";
-import styled from "styled-components";
-
-const Main = styled("div")`
-  font-family: sans-serif;
-  background: #ffffff;
-  height: 27vh;
-`;
-
-const DropDownContainer = styled("div")`
-  width: 10.5em;
-  margin: 0 auto;
-`;
-
-const DropDownHeader = styled("div")`
-  margin-bottom: 0.8em;
-  padding: 0.4em 2em 0.4em 1em;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
-  font-weight: 500;
-  font-size: 1.3rem;
-  color: #3faffa;
-  background: #ffffff;
-`;
-
-const DropDownListContainer = styled("div")``;
-
-const DropDownList = styled("ul")`
-  padding: 0;
-  margin: 0;
-  padding-left: 1em;
-  background: #ffffff;
-  border: 2px solid #e5e5e5;
-  box-sizing: border-box;
-  color: #3faffa;
-  font-size: 1.3rem;
-  font-weight: 500;
-  &:first-child {
-    padding-top: 0.8em;
-  }
-`;
-
-const ListItem = styled("li")`
-  list-style: none;
-  margin-bottom: 0.8em;
-`;
-
-const options = ["Recreation", "Personal", "Housing","Food","Insurance","Transport"];
-
-export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
-
-  const toggling = () => setIsOpen(!isOpen);
-
-  const onOptionClicked: (value:any) => () => {
-    setSelectedOption(value);
-    setIsOpen(false);
-    console.log(selectedOption);
-  };
-
-  return (
-    <Main>
-      <h1>Category</h1>
-      <DropDownContainer>
-        <DropDownHeader onClick={toggling}>
-          {selectedOption || "Personal"}
-        </DropDownHeader>
-        {isOpen ? (
-          <DropDownListContainer>
-            <DropDownList>
-              {options.map(option => (
-                <ListItem onClick={onOptionClicked(option)} key={Math.random()}>
-                  {option}
-                </ListItem>
-              ))}
-            </DropDownList>
-          </DropDownListContainer>
-        ) : []}
-      </DropDownContainer>
-    </Main>
+  return(
+    <Dropdown>
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      Dropdown Button
+    </Dropdown.Toggle>
+  
+    <Dropdown.Menu>
+      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
   );
-}*/
+
+}
